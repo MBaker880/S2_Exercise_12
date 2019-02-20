c005039cb65494526e940c427bb4fc168db2336c
Kenzie Baker <mbaker880@west-mec.org>
2019-02-19 18:23:15 -0700

    Added to JaveScript file, Changes to Calendar
    Created both local and global variables, created a function in which the values of each month were inputed.

--- lht_calendar.js
+++ lht_calendar.js
@@ -29,3 +29,21 @@
       Writes the daily rows in the calendar table, highlighting calDate
 	
 */
+/* Set the date diplayed in the calendar */
+var thisDay = new Date("August 24, 2018");
+
+// Write te calendar to the element with the id "calendar"
+document.getElementById("calendar").innerHTML = createCalendar(thisDay);
+
+// Finctio to generate the calendar table
+function createCalendar(calDate) {
+      var calendarHTML = "<table id='calendar_table'>";
+      calendarHTML += "</table>";
+      return calendarHTML;
+}
+
+// Function to write the calendar caption
+function calCaption(calDate) {
+      //monthName array contains the list of month names
+      var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+}
